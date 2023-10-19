ff81f88401ebfeeee50d507f8b925ddb08138c9f
Mateus Vieira <mateus.sly.mp@gmail.com>
2023-10-19 19:38:59 -0300

    intial commit

---
+++ src/test/java/com/mathsly/animelibrary/AnimeLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mathsly.animelibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
